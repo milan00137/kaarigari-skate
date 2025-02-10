a1dd2414d6069eda1dbf96f4b7b378102e629933
milan <milandhameliya0013@gmail.com>
2025-02-10 17:30:25 +0530

    Fixed Bounded typescript

--- src/app/components/Bounded.tsx
+++ src/app/components/Bounded.tsx
@@ -1,28 +1,28 @@
-import { CSSProperties, ElementType, ReactNode } from "react";
+import { CSSProperties, ElementType, ReactNode, ComponentPropsWithRef } from "react";
 import clsx from "clsx";
 
-type BoundedProps = {
-    as?: ElementType;
-    className?: string;
-    style?: CSSProperties;
-    children: ReactNode;
-};
+type BoundedProps<T extends ElementType = "section"> = {
+  as?: T;
+  className?: string;
+  style?: CSSProperties;
+  children?: ReactNode;
+} & ComponentPropsWithRef<T>;
 
-export function Bounded({
-    as: Comp = "section",
-    className,
-    children,
-    ...restProps
-}: BoundedProps) {
-    return (
-        <Comp
-            className={clsx(
-                "px-6 ~py-10/16 [.header+&]:pt-44 [.header+&]:md:pt-32",
-                className,
-            )}
-            {...restProps}
-        >
-            <div className="mx-auto w-full max-w-6xl">{children}</div>
-        </Comp>
-    );
+export function Bounded<T extends ElementType = "section">({
+  as: Comp = "section",
+  className,
+  children,
+  ...restProps
+}: BoundedProps<T>) {
+  return (
+    <Comp
+      className={clsx(
+        "px-6 ~py-10/16 [.header+&]:pt-44 [.header+&]:md:pt-32",
+        className
+      )}
+      {...restProps}
+    >
+      <div className="mx-auto w-full max-w-6xl">{children}</div>
+    </Comp>
+  );
 }
